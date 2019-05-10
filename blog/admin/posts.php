43bda70acd542138f92d92d10b01b47a4c1e9e49
Ramy Ibrahim <ramymibrahim@hotmail.com>
2019-05-10 12:51:47 +0200

    Admin.Posts

---
+++ blog/admin/posts.php
@@ -0,0 +1,63 @@
+<?php
+require_once('../helpers/config.php');
+require_once($base_path.'helpers/users.php');
+protect();
+require_once($base_path.'layouts/header.php');
+require_once($base_path.'helpers/posts.php');
+$posts = getPosts();
+?>
+<style>
+
+</style>
+   <!-- Page Header -->
+   <header class="masthead" style="background-image: url('<?php echo $base_url?>img/home-bg.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>Posts</h1>              
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+<div class="container">
+<div class="row">
+        <div class="col-md-12">
+        <a href="<?php echo $base_url?>admin/addPost.php" class="btn btn-success">Add</a>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th style="width: 10%;">id</th>
+                        <th>Title</th>
+                        <th>User Name</th>
+                        <th>Category Name</th>
+                        <th colspan="2" style="width: 10%;">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach($posts as $post){
+                    ?>
+                          
+                    <tr>
+                        <td scope="row"><?php echo $post['id'];?></td>
+                        <td><?php echo $post['title'];?></td>
+                        <td><?php echo $post['posted_by'];?></td>
+                        <td><?php echo $post['category_name'];?></td>
+                        <td><a class="btn btn-primary" href="<?php echo $base_url?>admin/editPost.php?id=<?php echo $cat['id'];?>">Edit</a></td>
+                        <td><button class="btn btn-danger">Delete</button></td>
+                    </tr>     
+                    <?php
+                }
+                ?>             
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+  
+<?php
+require_once('../layouts/footer.php');
+?>
--- blog/helpers/posts.php
+++ blog/helpers/posts.php
@@ -1,9 +1,15 @@
 <?php
 require_once($base_path.'helpers/database.php');
 function getPosts($cat_id=null){
-    $q="SELECT posts.*,users.name as posted_by FROM posts inner join users on users.id=posts.user_id WHERE 1=1";
+    $q="SELECT posts.*
+    ,users.name as posted_by
+    ,categories.name as category_name
+     FROM posts 
+     inner join users on users.id=posts.user_id 
+     inner join categories on categories.id=posts.category_id 
+     WHERE 1=1";
     if($cat_id!=null){
         $q=$q." AND category_id=$cat_id";
-    }    
+    }
     return getRows($q);
 }
